a17b5800a5c76e396fb158624662d0d87cf8b82b
Jeevan Kumar <jeevankumarguduru3@gmail.com>
2026-04-06 12:38:26 +0530

    New function is added for operations

--- calculator.py
+++ calculator.py
@@ -12,25 +12,32 @@ def div(a,b):
 def mul(a,b):
     return a*b
 
+def call(a,b,op):
+    if op == "/" and b == 0:
+        print("ERROR, Divission not possible with 0")
+
+    else:
+        if op == "+":
+            res = sum(a,b)
+
+        if op == "-":
+            res = diff(a,b)
+
+        if op == "*":
+            res = mul(a,b)
+
+        if op == "/":
+            res = div(a,b)
+        
+        print("Result : ",res)
+    
+
 a = float(input("Enter the first number : "))
 op = input("Enter the operator : ")
 b = float(input("Enter the second number : "))
 
-if op == "/" and b == 0:
-    print("ERROR, Divission not possible with 0")
+call(a,b,op)
 
-else:
-    if op == "+":
-        res = sum(a,b)
 
-    if op == "-":
-        res = diff(a,b)
 
-    if op == "*":
-        res = mul(a,b)
-
-    if op == "/":
-        res = div(a,b)
-    
-    print("Result : ",res)
 
